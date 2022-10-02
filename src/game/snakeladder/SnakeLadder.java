dbd01157c748ce791f98a439146119fa0e186828
sayrish <sayrrish1@gmail.com>
2022-10-02 13:24:28 +0530

    Print Statement for (No Play, Ladder, Snake)

--- src/game/snakeladder/SnakeLadder.java
+++ src/game/snakeladder/SnakeLadder.java
@@ -12,7 +12,25 @@ public class SnakeLadder {
 		System.out.println("Initial position of user: "+userPosition);
 		
 		int dice = r.nextInt(6) + 1 ;
-		System.out.println("Dice Rolled: "+dice);	
+		System.out.println("Dice Rolled: "+dice);
+		
+		int option = r.nextInt(3);
+		switch(option)
+		{
+		case 0: System.out.println("No Play!!");
+				break;
+		case 1: System.out.println("Ladder!");
+				userPosition = userPosition + dice;
+				
+				break;
+		default: System.out.println("Snake!");
+				userPosition = userPosition - dice;
+				
+				if(userPosition<0)
+				{ userPosition = 0; 
+				}
+		}
+		System.out.println("Presnt Position of the User: "+userPosition);	
 		
 	}
 
