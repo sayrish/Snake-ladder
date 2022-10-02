498f2d8ffe3b241d2b2f246bb040e028a7bcc0a6
sayrish <sayrrish1@gmail.com>
2022-10-02 13:42:26 +0530

    number of times user roll the dice

--- src/game/snakeladder/SnakeLadder.java
+++ src/game/snakeladder/SnakeLadder.java
@@ -10,34 +10,37 @@ public class SnakeLadder {
 		int userPosition = 0;
 		int count = 0;
 		System.out.println("----Game Starts----");
-		System.out.println("Initial position of user: "+userPosition);
-		
-		while(userPosition != 100)
-		{
+		System.out.println("Initial position of user: " + userPosition);
+
+		while (userPosition != 100) {
 			count++;
-		int dice = r.nextInt(6) + 1 ;
-		System.out.println("Dice Rolled: "+dice);
-		
-		int option = r.nextInt(3);
-		switch(option)
-		{
-		case 0: System.out.println("No Play!!");
+			int dice = r.nextInt(6) + 1;
+			System.out.println("Dice Rolled: " + dice);
+
+			int option = r.nextInt(3);
+			switch (option) {
+			case 0:
+				System.out.println("No Play!!");
 				break;
-		case 1: System.out.println("Ladder!");
+			case 1:
+				System.out.println("Ladder!");
 				userPosition = userPosition + dice;
-				
-				break;
-		default: System.out.println("Snake!");
-				userPosition = userPosition - dice;
-				
-				if(userPosition<0)
-				{ 
-					userPosition = 0; 
+				if (userPosition > 100) {
+					userPosition -= dice;
 				}
+				break;
+			default:
+				System.out.println("Snake!");
+				userPosition = userPosition - dice;
+
+				if (userPosition < 0) {
+					userPosition = 0;
+				}
+			}
+			System.out.println("Number of times user played "+count+" to reach: "+userPosition);
+			System.out.println("Present Position of the User: " + userPosition);
 		}
-		System.out.println("Presnt Position of the User: "+userPosition);
-		}
-		System.out.println("User has rolled "+count+" times to win!")
+		System.out.println("User has rolled " + count + " times to win!");
 	}
 
 }
